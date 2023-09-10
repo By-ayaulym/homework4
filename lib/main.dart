df06f61ee27561c38e0667b767fe662524f72fff
By-ayaulym <144624776+By-ayaulym@users.noreply.github.com>
2023-09-11 02:22:39 +0600

    qaz

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(backgroundColor: Colors.pink,),
-      body: Container(color: Colors.purple, child: Text("jjjj")),)
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple, 
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.pink, 
+                child: Text(
+                  "Hello",
+                  style: TextStyle(
+                    fontSize: 34,
+                    color: Colors.white, 
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                color: Colors.purple, 
+                child: Text(
+                  "20.810 digital marketing",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black, 
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
